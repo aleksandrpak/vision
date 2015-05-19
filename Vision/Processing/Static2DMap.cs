eabd396c1168ba4b777f192297f647201c3d4ba2
Aleksandr Pak <alekspak@icloud.com>
2015-05-19 18:19:31 +0300

    first changes for colored map

--- Vision/Processing/Static2DMap.cs
+++ Vision/Processing/Static2DMap.cs
@@ -5,7 +5,7 @@ namespace Vision.Processing
 {
     public sealed class Static2DMap
     {
-        private readonly ushort[] _map;
+        private readonly List<Tuple<ushort, byte, byte>>[] _map;
 
         private double _currentAngle;
 
@@ -16,7 +16,7 @@ namespace Vision.Processing
             HorizontalAngle = horizontalAngle;
             MaxDepth = maxDepth;
 
-            _map = new ushort[(int)(360.0 / (HorizontalAngle / Width))];
+            _map = new List<Tuple<ushort, byte, byte>>[(int)(360.0 / (HorizontalAngle / Width))];
 
             _currentAngle = 0;
         }
@@ -52,53 +52,80 @@ namespace Vision.Processing
                 minDepth = -minDepth;
                 minDepth = (int)(minDepth / Math.Sin((90 - screenAngle) * Math.PI / 180.0));
 
-                _map[(int)(angle / 360.0 * (_map.Length - 1))] = (ushort)minDepth;
+                var index = (int)(angle / 360.0 * (_map.Length - 1));
+
+                if (_map[index] == null)
+                    _map[index] = new List<Tuple<ushort, byte, byte>>();
+                else
+                    _map[index].Clear();
+
+                _map[index].Add(new Tuple<ushort, byte, byte>((ushort)minDepth, 255, 0));
             }
 
             if (MapImageUpdated == null)
                 return;
 
             var width = (double)MaxDepth * 2 / 10;
-            var pixels = new byte[(int)width * (int)width];
+            var pixels = new byte[(int)width * (int)width * 3];
 
             for (var i = 0.0; i < _map.Length; ++i)
             {
-                var depth = _map[(int)i] / 10;
-                if (depth == 0)
+                var depths = _map[(int)i];
+                if (depths == null)
                     continue;
 
-                var angle = i / _map.Length * 2 * Math.PI + 2.181661565; // + 125 degrees
-                var x = (int)((width / 2) + Math.Cos(angle) * depth);
-                var y = (int)((width / 2) - Math.Sin(angle) * depth);
+                for (var j = 0; j < depths.Count; ++j)
+                {
+                    var item = _map[(int)i][j];
+                    var depth = item.Item1 / 10;
+                    if (depth == 0)
+                        continue;
 
-                SetPixels(pixels, (int)width, x, y);
+                    var angle = i / _map.Length * 2 * Math.PI + 2.181661565; // + 125 degrees
+                    var x = (int)((width / 2) + Math.Cos(angle) * depth);
+                    var y = (int)((width / 2) - Math.Sin(angle) * depth);
+
+                    SetPixels(pixels, (int)width, x, y, item.Item2, item.Item3);
+                }
             }
 
             RaiseMapImageUpdated(pixels, (int)width, (int)width);
         }
 
-        private static void SetPixels(IList<byte> pixels, int width, int x, int y)
+        private static void SetPixels(IList<byte> pixels, int width, int x, int y, byte green, byte red)
         {
             if (y - 1 > 0)
-                SetPixelsRow(pixels, width, x, y - 1);
+                SetPixelsRow(pixels, width, x, y - 1, green, red);
 
-            SetPixelsRow(pixels, width, x, y);
+            SetPixelsRow(pixels, width, x, y, green, red);
 
-            if ((y + 1) * width < pixels.Count)
-                SetPixelsRow(pixels, width, x, y + 1);
+            if ((y + 1) < width)
+                SetPixelsRow(pixels, width, x, y + 1, green, red);
         }
 
-        private static void SetPixelsRow(IList<byte> pixels, int width, int x, int y)
+        private static void SetPixelsRow(IList<byte> pixels, int width, int x, int y, byte green, byte red)
         {
-            var firstPixel = y * width + (x - 1);
+            // b g r
+            var firstPixel = y * width * 3 + ((x - 1) * 3);
             if (firstPixel > 0)
-                pixels[firstPixel] = 255;
+            {
+                pixels[firstPixel] = 0;
+                pixels[firstPixel + 1] = green;
+                pixels[firstPixel + 2] = red;
+            }
 
-            pixels[y * width + x] = 255;
+            var secondPixel = y * width * 3 + (x * 3);
+            pixels[secondPixel] = 0;
+            pixels[secondPixel + 1] = green;
+            pixels[secondPixel + 2] = red;
 
-            var thirdPixel = y * width + (x + 1);
+            var thirdPixel = y * width * 3 + ((x + 1) * 3);
             if (thirdPixel < pixels.Count)
-                pixels[thirdPixel] = 255;
+            {
+                pixels[thirdPixel] = 0;
+                pixels[thirdPixel + 1] = green;
+                pixels[thirdPixel + 2] = red;
+            }
         }
 
         private static double NormalizeAngle(double angle)
@@ -119,8 +146,8 @@ namespace Vision.Processing
                 DpiX = 96.0,
                 DpiY = 96.0,
                 Pixels = pixels,
-                Stride = width,
-                BitsPerPixel = 8
+                Stride = width * 3,
+                BitsPerPixel = 24
             });
         }
 
